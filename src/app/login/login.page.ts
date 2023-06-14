8230f1b591535a38d4ebbfeb917726674ae19415
Hasan Seirafi <109631351+wujimu@users.noreply.github.com>
2023-06-14 13:32:53 -0700

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,49 +1,37 @@
-import { ionForm, IonForm, DynamicFormType, FieldValidationResponse } from 'ionform';
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router } from '@angular/router';
-
+import {
+  ionForm,
+  IonForm,
+  DynamicFormType,
+  FieldValidationResponse,
+} from "ionform";
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
 export class LoginPage {
   email!: string;
   password!: string;
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
 
-  async ngAfterViewInit() {
-
-
-
-  }
-
+  async ngAfterViewInit() {}
 
   async ngOnInit() {
+    const myForm: IonForm = ionForm("myform");
 
-      const myForm: IonForm = ionForm("myform");
+    const stringKeyValuePairs = {
+      password: "mypassword",
 
-      const stringKeyValuePairs =
+      repeatPassword: "",
+    };
 
-      {
-
-        "password": 'mypassword',
-
-        "repeatPassword": ''
-
-      };
-
-      myForm.createForm({ jsonForm: stringKeyValuePairs });
-
-
-
-
-
+    myForm.createForm({ jsonForm: stringKeyValuePairs });
   }
-
 
   // async login() {
   //   try {
