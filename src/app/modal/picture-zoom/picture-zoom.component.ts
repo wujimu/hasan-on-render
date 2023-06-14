854e3d042a40323896bb1557c849ded7b481dcce
Hasan Seirafi <109631351+wujimu@users.noreply.github.com>
2023-06-14 13:33:41 -0700

    Update picture-zoom.component.ts

--- src/app/modal/picture-zoom/picture-zoom.component.ts
+++ src/app/modal/picture-zoom/picture-zoom.component.ts
@@ -1,23 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, Input, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-picture-zoom',
-  templateUrl: './picture-zoom.component.html',
-  styleUrls: ['./picture-zoom.component.scss'],
+  selector: "app-picture-zoom",
+  templateUrl: "./picture-zoom.component.html",
+  styleUrls: ["./picture-zoom.component.scss"],
 })
 export class PictureZoomComponent implements OnInit {
- @Input() photo!: any;
-  name!:string;
-  constructor(private modalCtl: ModalController) { }
+  @Input() photo!: any;
+  name!: string;
+  constructor(private modalCtl: ModalController) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
   cancel() {
-    return this.modalCtl.dismiss(null, 'cancel');
+    return this.modalCtl.dismiss(null, "cancel");
   }
   confirm() {
-    return this.modalCtl.dismiss(this.name, 'confirm');
-
+    return this.modalCtl.dismiss(this.name, "confirm");
   }
-
 }
